5984f43a107704d133361023cb06c7848fad4f08
Miru <Miruna.MOCANU@business-school.academy>
2025-03-16 22:09:34 +0100

    jsp si jai commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from objects.game import MatchaGame
 def main():
     pygame.init()
     game = MatchaGame()
-    game.game_state = "start_screen"  # Set initial state explicitly
-    game.running = True  # Make sure running is True
+    game.game_state = "start_screen"
+    game.running = True
     game.run()
     pygame.quit()
 
